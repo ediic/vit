0ad073232d43f0ea74e3a8c453982d916278d700
ediic <ediicediic@gmail.com>
2021-04-06 23:11:12 +0300

    footer

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    return view('test');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/dashboard', function () {
